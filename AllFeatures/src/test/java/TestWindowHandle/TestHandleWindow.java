84037a2169b3905612ae4644676f469acbebf0e8
mdtaque <rifattaque@gmail.com>
2018-10-12 02:10:04 -0400

    finalll

---
+++ AllFeatures/src/test/java/TestWindowHandle/TestHandleWindow.java
@@ -0,0 +1,42 @@
+package TestWindowHandle;
+
+import WindowHandling.HandleWindow;
+import base.BaseUtil;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestHandleWindow extends BaseUtil {
+
+    HandleWindow objOfHandleWindow;
+
+    @BeforeMethod
+    public void init(){
+        objOfHandleWindow = PageFactory.initElements(driver, HandleWindow.class);
+        driver.get("http://toolsqa.com/automation-practice-switch-windows/");
+    }
+    @Test
+    public void testWindowHandling() throws InterruptedException {
+        objOfHandleWindow.handlingWindow();
+    }
+    @Test
+    public void testWindowHandleByCondition() throws InterruptedException {
+        objOfHandleWindow.windowSwitchByCondition();
+    }
+    @Test
+    public void switchAgainnn() throws InterruptedException {
+        objOfHandleWindow.switchWindowAgain();
+    }
+    @Test
+    public void switchMsgWindow() throws InterruptedException {
+        objOfHandleWindow.switchToMsgWindow();
+    }
+    @Test
+    public void switchTab() throws InterruptedException {
+        objOfHandleWindow.switchToNewTab();
+    }
+    @Test
+    public void handleAlertBox() throws InterruptedException {
+        objOfHandleWindow.handleAlertBox();
+    }
+}
